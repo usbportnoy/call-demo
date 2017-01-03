de46ff7f1cf081aca06d6433d0327cd0e2ae3533
Jay Port <jp@notreal.com>
2017-01-03 01:04:41 -0500

    initial commit

---
+++ src/main/java/com/bbt/usbportnoy/CallDemoApplication.java
@@ -0,0 +1,12 @@
+package com.bbt.usbportnoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallDemoApplication.class, args);
+	}
+}
